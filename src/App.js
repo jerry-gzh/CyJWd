0094f68130756dd12e772e3768a25cddf65328f5
Gerardo G <gerardoa.guzmanh@gmail.com>
2023-08-11 18:06:32 -0600

    Agregando basename

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/home/home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" basename="/tothepoint_login">
       <Home/>
     </div>
   );
